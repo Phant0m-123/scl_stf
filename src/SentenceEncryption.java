69bb03b25cc88ab63f36f1e570f4ba7ef38e227a
Phant0m-123 <adit.rawat.gamer@gmail.com>
2024-11-04 20:08:29 +0530

    Question 15

--- src/SentenceEncryption.java
+++ src/SentenceEncryption.java
@@ -4,16 +4,8 @@ public class SentenceEncryption {
 
     private String[] words;
 
-    void getWords(String Sentence) {
-        int count = 0;
-        for (int i = 0; i <= Sentence.length(); i++) {
-            count += (Sentence.charAt(i) == ' ') ? 1 : 0;
-        }
-        words = new String[count+1];
-        int lastIndexOfSpace = 0;
-        for (int i = 0; i <= Sentence.length(); i++) {
-            
-        }
+    void encrypt(int n) {
+
     }
 
 }
